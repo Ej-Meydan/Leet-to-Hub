db7caa44636542e20d8274e2326c787e6fdb2932
Emre Meydan <ej.meydan@outlook.com>
2026-01-08 08:35:28 +0000

    Adding submission detection, multi submission method support and result detection

--- content.js
+++ content.js
@@ -1,1 +1,39 @@
-console.log("Hello world!");
+let waitingForResult = false;
+
+function submissionHandler(event) {
+    const submitButton = event.target.closest(
+        '[data-e2e-locator="console-submit-button"]'
+    );
+
+    const ctrlEnter = event.type === "keydown" && event.ctrlKey && event.key === "Enter";
+
+    if (submitButton || ctrlEnter) {
+        waitingForResult = true;
+        console.log("SUBMISSION STARTED - waiting for result");
+    }
+}
+
+document.addEventListener("click", submissionHandler);
+document.addEventListener("keydown", submissionHandler);
+
+const observer = new MutationObserver(() => {
+    const result = document.querySelector(
+        '[data-e2e-locator="submission-result"]'
+    );
+
+    if (!result) return;
+
+    if (!waitingForResult) return;
+
+    if (result.innerText.includes("Accepted")) {
+        console.log("ACCEPTED DETECTED");
+        
+        waitingForResult = false;
+        observer.disconnect();
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true 
+});
